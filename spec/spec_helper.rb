7277371168cac89d3691b6dcc29342d6a866d1a2
Oleg Ivanov <morhekil@morhekil.net>
2014-01-22 12:04:01 +1100

    set up rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 require 'paperbin'
 
+RSpec.configure do |config|
+  config.mock_with :rspec
+
+  config.filter_run focus: true
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.alias_example_to :fit, focus: true
+end
